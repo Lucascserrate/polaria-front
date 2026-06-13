b60748fdf63caa45e9a9a2eba345a57d2916e1d5
Lucas Camacho <Lucascserrate@gmail.com>
2026-06-13 16:16:20 -0400

    Trying to fix auth

--- components/GoogleButton.tsx
+++ components/GoogleButton.tsx
@@ -1,16 +1,15 @@
-import Link from 'next/link';
 import { API_BASE_URL } from '@/constants/env';
 import { FcGoogle } from 'react-icons/fc';
 
 const GoogleButton: React.FC = () => {
 	return (
-		<Link
+		<a
 			href={`${API_BASE_URL}/auth/google`}
 			className="w-full flex justify-center items-center gap-2 py-2.5 rounded-[10px] shadow-[2px_4px_5px_#00000022] cursor-pointer"
 		>
 			<FcGoogle className="w-6 h-6 xl:w-7 xl:h-7" />
 			<div className="text-xs xl:text-sm font-medium">Continuar con Google</div>
-		</Link>
+		</a>
 	);
 };
 
